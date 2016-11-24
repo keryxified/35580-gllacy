9688fb2290d275feb4fe8f150c31e4d4e9d4ede0
keryxified <keryxified@gmail.com>
2016-11-25 02:44:17 +0300

    25.11

--- js/script.js
+++ js/script.js
@@ -14,3 +14,19 @@ close.addEventListener("click", function(event) {
 	popup.classList.remove("feedback-form-show");
 	overlay.classList.remove("modal-overlay-show");
 });
+function initialize() {
+	var mapOptions = {
+		zoom: 15,
+		center: new google.maps.LatLng(59.9386865,30.3207383)
+}
+var map = new google.maps.Map(document.getElementById('map-block'),
+mapOptions);
+var image = "img/map-pin.png";
+var myLatLng = new google.maps.LatLng(59.938,30.320);
+var beachMarker = new google.maps.Marker({
+		position: myLatLng,
+		map: map,
+		icon: image
+	});
+}
+google.maps.event.addDomListener(window, 'load', initialize);
